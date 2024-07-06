6002483cadd7aeb5e48d3c203d841e81bd3051b6
Schmeka <cho.eliezer@gmail.com>
2024-07-06 11:12:28 -0400

    debug

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ class App{
 
     resize(){
         this.stageWidth = document.body.clientWidth;
-        this.stageHeight = document.body.stageHeight;
+        this.stageHeight = document.body.clientHeight;
 
         this.canvas.width = this.stageWidth * 2;
         this.canvas.height = this.stageHeight * 2;
