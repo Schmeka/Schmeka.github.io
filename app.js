40349a2ff534c4ff752619e1be3b3f30929a17f1
Schmeka <cho.eliezer@gmail.com>
2024-07-07 15:36:28 -0400

    furina audio

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ function youtubevideo(){
 
 function furina(){
     window.open('https://akasha.cv/profile/655060329');
+    const audio = new Audio();
+    audio.src = "./let my name echo in song.mp3"
+    audio.play();
 }
