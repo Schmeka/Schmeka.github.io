4190ff32ae295c74592c6eeda9b08b4452e33182
Schmeka <cho.eliezer@gmail.com>
2024-07-08 16:17:04 -0400

    finished accordion

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ function furina(){
     audio.src = "./let my name echo in song.mp3"
     audio.play();
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = "200px";
+    }
+  });
+}
+
