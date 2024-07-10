083492cfeb1c75efb0284b4095272126e2e16b59
Schmeka <cho.eliezer@gmail.com>
2024-07-10 16:30:02 -0400

    added modal

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ function Home() {
 document.querySelector(".idk").addEventListener("click", function() {
     window.location.href = "newpage.html"; 
 });
+
+var modal = document.getElementById("furinamodal");
+
+function openmodal(){
+    modal.style.display ="block";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
